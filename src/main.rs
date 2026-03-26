b1ff19840edf3495679e2b0b4deb89807f789ae9
Ahmad <MohamadouAhmadou>
2026-03-26 21:53:09 +0100

    Subtraction function...

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::io;
 
 // USER INPUT FUNCTION
-fn user_input() -> i32{
+fn user_input() -> f32{
     let mut input = String::new();
 
     io::stdin().read_line(&mut input).expect("Failed to read line.");
 
-    input.trim().parse::<i32>().expect("Please enter a valid number")
+    input.trim().parse::<f32>().expect("Please enter a valid number")
 }
 
 // ADDITION FUNCTON
-fn addition(a: i32, b: i32) -> i32{
+fn addition(a: f32, b: f32) -> f32{
     return a + b;
 }
+
+// SUBTRACTION FUNCTION
+fn subtration(a: f32, b: f32) -> f32 {
+    return a - b;
+} 
 
 
 fn main() {
@@ -25,6 +30,7 @@ fn main() {
     let number2 = user_input();
 
     println!("The sum of {} and {} is {}.", number1, number2, addition(number1, number2));
+    println!("The subtracton of {} and {} is {}.", number1, number2, subtration(number1, number2));
 
     //After creating all functions(subtration, multiplication, and division), loop and match will be use
 }
